22906802a89783f480734619da16f862d352c336
Wahid Nory <wahid_kl@hotmail.com>
2016-01-26 14:17:08 +0100

    fixed pipeline format for the api

--- scraper/productnu_scraper/pipelines.py
+++ scraper/productnu_scraper/pipelines.py
@@ -11,6 +11,15 @@ import json
 class ProductnuScraperPipeline(object):
     def process_item(self, item, spider):
         url = 'http://198.199.125.55:3000/product/'
-        payload = json.dumps(dict(item))
+        payload = {
+            'description': item['description'],
+            'ean': item['ean'],
+            'price': item['price'],
+            'supply': item['supply'],
+            'name': item['name'],
+            'retailer_alias': item['retailer_alias'],
+            'product_page': item['product_page'],
+        }
+
         r = requests.post(url, json=payload)
         return r.text
